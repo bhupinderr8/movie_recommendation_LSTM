21f23967fed7d3c4aea94e0a7036d7abd226de96
bhupinderr8 <bhupinder37kumar@gmail.com>
2019-06-13 00:51:54 +0530

    update

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ function cleanup {
   sudo kill `sudo lsof -t -i:9200` &> /dev/null || true
   sudo kill `sudo lsof -t -i:850` &> /dev/null || true
   >server.log
-  >server_elastic.log
+  sudo -i service elasticsearch stop
 }
 trap cleanup EXIT
 
@@ -18,7 +18,7 @@ sudo kill `sudo lsof -t -i:850` &> /dev/null || true
 
 MODEL_DIR=/home/bhupinder/PycharmProjects/movie_recommendation_LSTM/coreapi/dataset
 
-/home/bhupinder/elasticsearch-6.5.4/bin/elasticsearch >server_elastic.log 2>&1 &
+sudo -i service elasticsearch start
 
 sudo tensorflow_model_server --rest_api_port=850 --model_name=model --model_base_path="${MODEL_DIR}" >server.log 2>&1 &
 
